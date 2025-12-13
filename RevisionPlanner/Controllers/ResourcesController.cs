2869b81e9d6d312368ee2099f7302f0d3b963407
mhadi2025 <mhadi.rh28@gmail.com>
2025-12-13 18:30:08 +0000

    Add EF Core controllers and user views

---
+++ RevisionPlanner/Controllers/ResourcesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RevisionPlanner.Data;
+using RevisionPlanner.Models;
+
+namespace RevisionPlanner.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private readonly RevisionPlannerDbContext _context;
+
+        public ResourcesController(RevisionPlannerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resources
+        public async Task<IActionResult> Index()
+        {
+            var revisionPlannerDbContext = _context.Resources.Include(r => r.Subject);
+            return View(await revisionPlannerDbContext.ToListAsync());
+        }
+
+        // GET: Resources/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .Include(r => r.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // GET: Resources/Create
+        public IActionResult Create()
+        {
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id");
+            return View();
+        }
+
+        // POST: Resources/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SubjectId,Title,Url")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resource);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id", resource.SubjectId);
+            return View(resource);
+        }
+
+        // GET: Resources/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id", resource.SubjectId);
+            return View(resource);
+        }
+
+        // POST: Resources/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SubjectId,Title,Url")] Resource resource)
+        {
+            if (id != resource.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resource);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResourceExists(resource.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id", resource.SubjectId);
+            return View(resource);
+        }
+
+        // GET: Resources/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resource = await _context.Resources
+                .Include(r => r.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return View(resource);
+        }
+
+        // POST: Resources/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource != null)
+            {
+                _context.Resources.Remove(resource);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResourceExists(int id)
+        {
+            return _context.Resources.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RevisionPlanner/Controllers/TimetablesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RevisionPlanner.Data;
+using RevisionPlanner.Models;
+
+namespace RevisionPlanner.Controllers
+{
+    public class TimetablesController : Controller
+    {
+        private readonly RevisionPlannerDbContext _context;
+
+        public TimetablesController(RevisionPlannerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Timetables
+        public async Task<IActionResult> Index()
+        {
+            var revisionPlannerDbContext = _context.Timetables.Include(t => t.Subject).Include(t => t.User);
+            return View(await revisionPlannerDbContext.ToListAsync());
+        }
+
+        // GET: Timetables/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var timetable = await _context.Timetables
+                .Include(t => t.Subject)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (timetable == null)
+            {
+                return NotFound();
+            }
+
+            return View(timetable);
+        }
+
+        // GET: Timetables/Create
+        public IActionResult Create()
+        {
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Timetables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,TimeTableDay,SlotNumber,SubjectId,Status")] Timetable timetable)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(timetable);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id", timetable.SubjectId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", timetable.UserId);
+            return View(timetable);
+        }
+
+        // GET: Timetables/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var timetable = await _context.Timetables.FindAsync(id);
+            if (timetable == null)
+            {
+                return NotFound();
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id", timetable.SubjectId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", timetable.UserId);
+            return View(timetable);
+        }
+
+        // POST: Timetables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,TimeTableDay,SlotNumber,SubjectId,Status")] Timetable timetable)
+        {
+            if (id != timetable.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(timetable);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TimetableExists(timetable.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Id", timetable.SubjectId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", timetable.UserId);
+            return View(timetable);
+        }
+
+        // GET: Timetables/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var timetable = await _context.Timetables
+                .Include(t => t.Subject)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (timetable == null)
+            {
+                return NotFound();
+            }
+
+            return View(timetable);
+        }
+
+        // POST: Timetables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var timetable = await _context.Timetables.FindAsync(id);
+            if (timetable != null)
+            {
+                _context.Timetables.Remove(timetable);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TimetableExists(int id)
+        {
+            return _context.Timetables.Any(e => e.Id == id);
+        }
+    }
+}
